cf662fbb0eb0b77d8078099f2d4a891f19a98a15
Nathan Henninger <nathanhenninger@u.boisestate.edu>
2016-06-15 17:12:24 -0700

    Now selects a subset of cards and adds them to the current game deck

--- ShinkeiSuijaku/Firefox/data/shinkeisuijaku.js
+++ ShinkeiSuijaku/Firefox/data/shinkeisuijaku.js
@@ -1,9 +1,14 @@
-var lessonNum = 1;
-var currBoard = [];
-var currFlippedCard = {};
-var boardDimension = 4;
+function CardException(message) {
+   this.message = message;
+   this.name = "CardException";
+}
 
-// Populate currBoard[] with random entries from the JSON file
+var lessonNum = 1;
+var currCardSet = []; // A random subset from the lesson file
+var currBoard = []; // Twice size of currCardSet - one for each half of flash card
+var boardDimension = 4; // TODO: add a listener to update this
+
+// Populate currCardSet[] with random entries from the JSON file
 function loadLesson() {
   lessonNum = parseInt(document.getElementById("lesson_select").value);
   var request = new XMLHttpRequest();
@@ -22,35 +27,55 @@ function loadLesson() {
       boardDimension = Math.floor(Math.sqrt(potentialCards.length));
     }
     for (i = 0; i < boardDimension * boardDimension / 2; i++) {
-      currBoard[i] = potentialCards[randomIndices[i]];
+      currCardSet[i] = potentialCards[randomIndices[i]];
     }
   };
   request.send(null);
 }
 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function pickRandomIndices(len) {
-  var rval = [];
-  var tempVal, randomIndex;
-  var currIndex = len;
-  for (i = 0; i < len; i++) {
-    rval[i] = i;
-  }
+function shuffle(array) {
+  var currIndex = array.length, tempVal, randomIndex;
   // While there remain elements to shuffle...
   while (currIndex !== 0) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currIndex);
     currIndex--;
     // And swap it with the current element.
-    tempVal = rval[currIndex];
-    rval[currIndex] = rval[randomIndex];
-    rval[randomIndex] = tempVal;
+    tempVal = array[currIndex];
+    array[currIndex] = array[randomIndex];
+    array[randomIndex] = tempVal;
   }
+  return array;
+}
+
+function pickRandomIndices(len) {
+  var rval = [];
+  for (i = 0; i < len; i++) {
+    rval[i] = i;
+  }
+  rval = shuffle(rval);
   return rval;
 }
 
-function drawBoard() {
+function prepCards() {
+  var randomCardLocations = [];
+  for (i = 0; i < currCardSet.length; i++) {
+    if (currCardSet[i].kana) {
+      currBoard[i].part1 = currCardSet[i].kana;
+      currBoard[i + currCardSet.length].part2 = currCardSet[i].pronunciation;
+    } else if (currCardSet[i].character) {
+      currBoard[i].part1 = currCardSet[i].character;
+      currBoard[i + currCardSet.length].part2 = currCardSet[i].meaning;
+    } else {
+      throw new CardException("InvalidCardSet");
+    }
+  }
+  currBoard = shuffle(currBoard);
+}
 
+function drawBoard() {
+  for ()
 }
 
 function playGame() {
@@ -58,3 +83,6 @@ function playGame() {
 
   }
 }
+
+// https://jsfiddle.net/m1erickson/sAFku/
+//https://stackoverflow.com/questions/20060915/javascript-how-do-you-set-the-value-of-a-button-with-an-element-from-an-array
