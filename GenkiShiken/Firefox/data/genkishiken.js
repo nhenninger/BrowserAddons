e1a89615724c3b8e260e2ac60d78a5a49eb238c7
Nathan Henninger <nathanhenninger@u.boisestate.edu>
2016-08-18 19:45:10 -0700

    Now loads JSON data and procedurally generates questions.

--- GenkiShiken/Firefox/data/genkishiken.js
+++ GenkiShiken/Firefox/data/genkishiken.js
@@ -1,19 +1,110 @@
 /*jshint esversion: 6 */
 var numCorrect = 0;
 var characterArray = [];
-var answerSuperset = [];
+var answerArray = [];
 const NUM_OF_LESSONS = 23;
+const NUM_OF_MULTI_CHOICE = 5;
 
+function QuestionException(message) {
+   this.message = message;
+   this.name = "QuestionException";
+}
 function LessonException(message) {
    this.message = message;
    this.name = "LessonException";
 }
 
-function displayQuestions() {
-  numCorrect = 0;
-  $("#exam").empty();
-  characterArray.shuffle();
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(arr){
+  var currentIndex = arr.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = arr[currentIndex];
+    arr[currentIndex] = arr[randomIndex];
+    arr[randomIndex] = temporaryValue;
+  }
+  return arr;
+}
 
+function createQuestionChoices(typeChoice, questionIndex, indices) {
+  for (var k = 0; k < indices.length; k++) {
+    // TODO
+    $("#question" + questionIndex + " .questionChoices")
+      .append(
+        $("<li/>")
+          .append(
+            $("<label/>")
+              .append(
+                $("<input/>", {
+                "type" : "radio",
+                "name": "question" + questionIndex,
+                "value" : characterArray[indices[k]][typeChoice]})
+              )
+              .append(characterArray[indices[k]][typeChoice])
+          )
+      );
+  }
+}
+
+function chooseYomi() {
+  return Math.random() <= 0.5 ? "on_yomi" : "kun_yomi";
+}
+
+function createQuestionText(index) {
+  var rand;
+  var choiceIndices = [];
+  choiceIndices[0] = index;
+  for (var j = 1; j < NUM_OF_MULTI_CHOICE; j++) {
+    do {
+      rand = Math.floor(Math.random() * characterArray.length);
+    } while (rand == index);
+    choiceIndices[j] = rand;
+  }
+  shuffle(choiceIndices);
+  if (characterArray[0].kana) { // Syllabary
+    $("input:radio[name=lessonFocus]").attr("disabled", true);
+    $("#question" + index + " h1").append(characterArray[index].kana);
+    answerArray[index] = characterArray[index].pronunciation;
+    createQuestionChoices("pronunciation", index, choiceIndices);
+  } else if (characterArray[0].character) { // Kanji
+    $("input:radio[name=lessonFocus]").attr("disabled", false);
+    $("#question" + index + " h1").append(characterArray[index].character);
+    if ($("input:radio[name=lessonFocus]:checked").val() === "meaning") {
+      answerArray[index] = characterArray[index].meaning;
+      createQuestionChoices("meaning", index, choiceIndices);
+    } else if ($("input:radio[name=lessonFocus]:checked").val() === "reading") {
+      answerArray[index] = characterArray[index][chooseYomi()];
+      createQuestionChoices(chooseYomi(), index, choiceIndices);
+    } else {
+      console.log("questionException displaying kanjiMeaningOrReading text");
+      throw new QuestionException("InvalidMeaningOrReading");
+    }
+  } else {
+    console.log("questionException displaying question text");
+    throw new QuestionException("InvalidQuestionSet");
+  }
+}
+
+function createQuestions() {
+  numCorrect = 0;
+  var exam = $("#exam_div");
+  exam.empty();
+  shuffle(characterArray);
+  for (var i = 0; i < characterArray.length; i++) {
+    exam.append(
+      $("<div/>", {"class": "questionDiv", "id": "question" + i})
+        .append(
+          $("<h1/>", {"class": "questionChar"})
+        ).append(
+          $("<ul>", {"class": "questionChoices"})
+        )
+    );
+    createQuestionText(i);
+  }
 }
 
 function loadJSON() {
@@ -28,15 +119,15 @@ function loadJSON() {
     dataType: "json",
     url: "lessons/lesson" + lessonNum + ".json",  // TODO: change folder back to symlink before publishing
     success: function (response) {
-      if (lessonNum > 2) {
-        characterArray = response.kanji;
-      } else if (lessonNum > 0) {
-        characterArray = response.syllabary;
-      } else {
+      if (lessonNum < 0 || lessonNum > NUM_OF_LESSONS) {
         throw new LessonException("InvalidLessonNumber");
+      } else if (lessonNum > 2) {
+        characterArray = response.kanji;
+      } else {
+        characterArray = response.syllabary;
       }
       // console.log(JSON.stringify(characterArray));
-      displayQuestions();
+      createQuestions();
     }
   });
 }
@@ -51,30 +142,9 @@ function listLessons() {
   $("option:first").prop("selected", true);
 }
 
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-Array.prototype.shuffle = function(){
-  var i = this.length, randomIndex, tempVal;
-  // While there remain elements to shuffle...
-  while (i !== 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * i);
-    i--;
-    // And swap it with the current element.
-    tempVal = this[i];
-    this[i] = this[randomIndex];
-    this[randomIndex] = tempVal;
-  }
-};
-
-// function resetQuiz() {
-//   numCorrect = 0;
-//   characterArray.shuffle();
-//   // Start quiz from beginning
-// }
-
 $(document).ready(function(){
   listLessons();
   $("#lesson_select").change(loadJSON);
-  $("input[type=radio][name=lessonFocus]").change(displayQuestions);
+  $("input[type=radio][name=lessonFocus]").change(createQuestions);
   loadJSON();
 });
